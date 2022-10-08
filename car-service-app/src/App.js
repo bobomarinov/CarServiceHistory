37f357c09d040466f89e91285456d93c97e4a0f9
Boris Marinov <56587960+bobomarinov@users.noreply.github.com>
2022-10-08 22:12:23 +0300

    view history by id

--- car-service-app/src/App.js
+++ car-service-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import axios from 'axios';
 
 
@@ -9,6 +9,7 @@ import axios from 'axios';
 function App() {
   const [message, setMessage] = useState('');
   const [carData, setData] = useState('');
+ 
 
 
   const handleChange = event => {
@@ -16,20 +17,34 @@ function App() {
     console.log('Car ID field:', event.target.value);
   };
 
+  /*
+  useEffect(() => {
+    getServicehistory();
+  }, []);
+  */
 
-  const getServicehistory = async event => {
-    event.preventDefault();
-    console.log('Car ID:', message);
-    //GET request with cross origin headers
-    await fetch(`http://localhost:5000//api/events/car?CAR_ID=${message}`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      setData(data);
-      console.log(typeof(data));
-    }
-    )
+  const getServicehistory = () => {
+    axios
+    .get(`http://localhost:5000//api/events/car?CAR_ID=${message}`)
+    .then((res)=>{
+      setData(res.data);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
   };
+
+  const deleteServicehistory = (eventId) => {
+    axios
+    .delete(`http://localhost:5000/api/events?EVENT_ID=${eventId}`)
+    .then((res)=>{
+      console.log(res);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  };
+    
 
 
 
@@ -45,8 +60,23 @@ function App() {
       <button onClick={getServicehistory}>
         Show Service History
       </button>
-      <p>Service History</p>
-
+      <h2>Service History</h2>
+      {Object.keys(carData).map(function(keyName, keyIndex) {
+    		return (
+      			<div key={keyName}>
+              <h3>Event ID: {carData[keyName]["ID"]}</h3>
+              <p>Car ID: {carData[keyName]["CAR_ID"]}</p>
+              <p>Event Description: {carData[keyName]["EVENT_DESCRITION"]}</p>
+              <p>Event Date: {carData[keyName]["EVENT_DATE"]}</p>
+              <p>Event Mileage: {carData[keyName]["EVENT_MILLAGE"]}</p>
+              <p>Repeatable: {carData[keyName]["EVENT_REPEATABLE"]}</p>
+              <p>Next Date: {carData[keyName]["NEXT_EVENT_DATE"]}</p>
+              <p>Next Mileage: {carData[keyName]["NEXT_EVENT_MILLAGE"]}</p>
+              <button>Update</button>
+              <button onClick={deleteServicehistory(carData[keyName]["ID"])}>Delete</button>
+          		</div>
+    		)
+		})}
     </>
   );
 }
